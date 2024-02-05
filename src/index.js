279dfea62423aa78f72ebc6e1db76a841657908f
Volodymyr Vitiuk <122730435+VitiukVV@users.noreply.github.com>
2024-02-05 16:51:42 +0200

    apdate readme and favicon

--- src/index.js
+++ src/index.js
@@ -2,16 +2,16 @@ import { App } from 'components/App';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
+import { BrowserRouter } from 'react-router-dom';
+import { PersistGate } from 'redux-persist/integration/react';
 import './index.css';
 import { persistor, store } from './redux/store';
-import { PersistGate } from 'redux-persist/integration/react';
-import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
-        <BrowserRouter basename="/goit-react-hw-08-phonebook">
+        <BrowserRouter basename="/PhonebookApp">
           <App />
         </BrowserRouter>
       </PersistGate>
